18119447162ca807987c5070ace8744631401a9a
limxien20 <limxien20@gmail.com>
2021-10-12 16:07:14 +0800

    remove Sign Up on navigation bar

--- patients/index.php
+++ patients/index.php
@@ -62,7 +62,6 @@
           <li><a class="nav-link scrollto" href="#about">About</a></li>
           <li><a class="nav-link scrollto" href="#departments">Departments</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="nav-link scrollto" href="patientSignUp.php">Sign Up</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
